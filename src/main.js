42b800db0efe97ab3f5ee3ee43309d0cdd98662e
Mazin Abubeker <mazinabubeker1123@gmail.com>
2023-10-21 06:01:22 -0700

    Added fontawesome

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faArrowUp)
+
 const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(createPinia())
-
 app.mount('#app')
