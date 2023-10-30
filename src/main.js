d1dc9304d6d7ec1ba4101e64130565eb18c49e46
Mazin Abubeker <mazinabubeker1123@gmail.com>
2023-10-30 15:52:56 -0700

    Formatted with prettier

--- src/main.js
+++ src/main.js
@@ -5,7 +5,12 @@ import App from './App.vue'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faArrowUp, faArrowRightFromBracket, faCompactDisc, faList } from '@fortawesome/free-solid-svg-icons'
+import {
+  faArrowUp,
+  faArrowRightFromBracket,
+  faCompactDisc,
+  faList
+} from '@fortawesome/free-solid-svg-icons'
 
 library.add(faArrowUp, faArrowRightFromBracket, faCompactDisc, faList)
 
